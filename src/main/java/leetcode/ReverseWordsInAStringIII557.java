cb01d98ff68c674aa789761c7d9f045628d94a5c
htbkoo <h1017760@connect.hku.hk>
2018-02-09 08:08:35 +0800

    improved ReverseWordsInAStringIII557 - added 2 faster solutions (from 91ms down to 14ms further down to 10ms) leetCode

--- src/main/java/leetcode/ReverseWordsInAStringIII557.java
+++ src/main/java/leetcode/ReverseWordsInAStringIII557.java
@@ -4,15 +4,40 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class ReverseWordsInAStringIII557 {
-    public String reverseWords(String s) {
+    public String reverseWordsStream(String s) {
         return Arrays.stream(s.split(" ")).map(StringBuilder::new).map(StringBuilder::reverse).collect(Collectors.joining(" ")).trim();
+    }
+
+    public String reverseWords(String s) {
+        final String[] words = s.split(" ");
+        final StringBuilder sb = new StringBuilder();
+        final int length = words.length;
+        for (int i = 0; i < length; i++) {
+            final String word = words[i];
+            if (i > 0) {
+                sb.append(" ");
+            }
+            sb.append(reverse(word));
+        }
+        return sb.toString().trim();
+
+    }
+
+    private String reverse(String s) {
+        final int length = s.length();
+        final char[] old = s.toCharArray();
+        final char[] reversed = new char[length];
+        for (int i = length - 1; i >= 0; --i) {
+            reversed[i] = old[length - i - 1];
+        }
+        return new String(reversed);
     }
 
     public String reverseWordsOwnReverse(String s) {
         return Arrays.stream(s.split(" ")).map(this::reverse).collect(Collectors.joining(" "));
     }
 
-    private String reverse(String s) {
+    private String reverseSlow(String s) {
         final int length = s.length();
         final StringBuilder sb = new StringBuilder();
         final char[] chars = s.toCharArray();
