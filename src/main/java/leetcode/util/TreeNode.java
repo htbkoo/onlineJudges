fb7a778a151a40b3c82abac7af9ad5de72919930
htbkoo <h1017760@connect.hku.hk>
2018-02-10 23:19:48 +0800

    added util/TreeNode leetCode

---
+++ src/main/java/leetcode/util/TreeNode.java
@@ -0,0 +1,15 @@
+package leetcode.util;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
