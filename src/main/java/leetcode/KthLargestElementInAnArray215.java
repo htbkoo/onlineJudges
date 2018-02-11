e22b65e683809fc816971f3108960c5f7f791d66
Hey <hey20081234@gmail.com>
2018-02-11 12:09:11 +0800

    improved KthLargestElementInAnArray215.java (added 2 priorityQueue approach with O(N lg k) time/O(N) space) onlineJudges - leetCode

--- src/main/java/leetcode/KthLargestElementInAnArray215.java
+++ src/main/java/leetcode/KthLargestElementInAnArray215.java
@@ -1,9 +1,39 @@
 package leetcode;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class KthLargestElementInAnArray215 {
     public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i : nums) {
+            pq.offer(i);
+        }
+
+        final int times = nums.length - k;
+        for (int i = 0; i < times; i++) {
+            pq.poll();
+        }
+
+        return pq.peek();
+    }
+
+    public int findKthLargestSlowPriorityQueueByComparator(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((i, j) -> Integer.compare(j, i));
+
+        for (int i : nums) {
+            pq.offer(i);
+        }
+
+        for (int i = k - 1; i > 0; --i) {
+            pq.poll();
+        }
+
+        return pq.peek();
+    }
+
+    public int findKthLargestBySort(int[] nums, int k) {
         Arrays.sort(nums);
         return nums[nums.length - k];
     }
